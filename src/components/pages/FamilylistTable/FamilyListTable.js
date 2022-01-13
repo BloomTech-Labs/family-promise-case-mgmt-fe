7de1a902a25d3b3fd59cfc8003bfdc6185e4c32d
Polly Yospan <porgroovy@gmail.com>
2022-01-12 22:33:14 -0500

    create family list folder and a family list file to contain a table that wi ll show family list from database by using ant design

---
+++ src/components/pages/FamilylistTable/FamilyListTable.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Table } from 'antd';
+
+const FamilyListTable = () => {
+  const { Column, ColumnGroup } = Table;
+
+  const data = [
+    {
+      key: '1',
+      firstName: 'John',
+      lastName: 'Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      firstName: 'Jim',
+      lastName: 'Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      firstName: 'Joe',
+      lastName: 'Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
+
+  return (
+    <Table dataSource={data}>
+      <ColumnGroup title="Name">
+        <Column title="First Name" dataIndex="firstName" key="firstName" />
+        <Column title="Last Name" dataIndex="lastName" key="lastName" />
+      </ColumnGroup>
+      <Column title="Age" dataIndex="age" key="age" />
+      <Column title="Address" dataIndex="address" key="address" />
+    </Table>
+  );
+};
+
+export default FamilyListTable;
