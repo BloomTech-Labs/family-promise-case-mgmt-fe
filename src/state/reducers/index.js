78c8da3e93df9597b896a97eb83725395ebcad02
GenHero <51133244+GenHero@users.noreply.github.com>
2022-01-19 19:46:48 -0500

    created redux-store

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,4 +1,8 @@
 // import all of your reducers into this file, and export them back out. 
 // This allows for the simplification of flow when importing reducers into your actions throughout your app.
 
+import { combineReducers } from 'redux';
 
+const rootReducer = combineReducers({});
+
+export default rootReducer;
