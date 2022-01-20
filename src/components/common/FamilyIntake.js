4ebc5f16e0a3e345acd8e18df915215f0b37d8fe
Omarius <70237021+credleo95@users.noreply.github.com>
2022-01-19 20:16:14 -0500

    Finished form by adding change handlers for select options and added minimal inline styling.

--- src/components/common/FamilyIntake.js
+++ src/components/common/FamilyIntake.js
@@ -4,6 +4,7 @@ import { Form, Input, Button, Select, InputNumber } from 'antd';
 function FamilyIntake() {
   const { TextArea } = Input;
 
+  // Initial Empty Values for form
   const familyInput = {
     head_of_household: '',
     family_members: '',
@@ -15,52 +16,58 @@ function FamilyIntake() {
 
   const [familyInfo, setFamilyInfo] = useState(familyInput);
 
+  // Change Handler used for all fields except Select.
   const formChanges = event => {
-    setFamilyInfo({ ...familyInfo, [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    setFamilyInfo({ ...familyInfo, [name]: value });
   };
-
-  const handleSubmit = event => {
-    event.preventDefault();
+  // antd's answer to onSubmit
+  const onFinish = () => {
     console.log(familyInfo);
   };
+
   return (
     <div>
       <Form
         labelCol={{ span: 4 }}
         wrapperCol={{ span: 14 }}
         layout="horizontal"
+        onFinish={onFinish}
+        style={{ paddingTop: '2rem' }}
       >
         <Form.Item
           label="Head of Household"
-          name="head_of_household"
-          value={familyInput.head_of_household}
+          value={familyInfo.head_of_household}
           onChange={formChanges}
         >
-          <Input />
+          <Input name="head_of_household" />
         </Form.Item>
         <Form.Item
           label="Names of Family Members"
-          name="family_members"
-          value={familyInput.family_members}
+          value={familyInfo.family_members}
           onChange={formChanges}
         >
-          <TextArea rows={4} placeholder="Names of Family Members" />
+          <TextArea rows={4} name="family_members" />
         </Form.Item>
         <Form.Item
           label="Number of Family Members"
-          name="family_size"
-          value={familyInput.family_size}
+          value={familyInfo.family_size}
           onChange={formChanges}
         >
-          <InputNumber />
+          <InputNumber name="family_size" />
         </Form.Item>
         <Form.Item
           label="Education Level"
-          name="education_level"
-          value={familyInput.education_level}
+          value={familyInfo.education_level}
           onChange={formChanges}
         >
-          <Select>
+          <Select
+            value={familyInfo.education_level}
+            name="education_level"
+            onChange={value => {
+              setFamilyInfo({ ...familyInfo, education_level: value });
+            }}
+          >
             <Select.Option value="High School Graduate" />
             <Select.Option value="Some College" />
             <Select.Option value="College Graduate" />
@@ -68,25 +75,33 @@ function FamilyIntake() {
         </Form.Item>
         <Form.Item
           label="Prior History"
-          name="homeless_history"
-          value={familyInput.homeless_history}
+          value={familyInfo.homeless_history}
           onChange={formChanges}
         >
-          <TextArea rows={4} placeholder="Prior History" />
+          <TextArea rows={4} name="homeless_history" />
         </Form.Item>
         <Form.Item
           label="Employment Status"
-          name="employment"
-          value={familyInput.employment}
+          value={familyInfo.employment}
           onChange={formChanges}
         >
-          <Select>
+          <Select
+            name="employment"
+            onChange={value => {
+              setFamilyInfo({ ...familyInfo, employment: value });
+            }}
+          >
             <Select.Option value="Employed" />
             <Select.Option value="Unemployed" />
           </Select>
         </Form.Item>
         <Form.Item>
-          <Button htmlType="submit" onClick={handleSubmit}>
+          <Button
+            htmlType="submit"
+            style={{
+              marginLeft: '30%',
+            }}
+          >
             Submit
           </Button>
         </Form.Item>
