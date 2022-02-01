4d244a82a60887ba27189c5a885426207a02f7da
GenHero <51133244+GenHero@users.noreply.github.com>
2022-02-01 13:22:54 -0500

    populated navbar

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -10,6 +10,9 @@ function Navigation() {
         <Menu onClick={({key}) => setCurrent(key)} selectedKeys={[current]} mode="horizontal">
             <Menu.Item key="home"><Link to ="/"/>Home</Menu.Item>
             <Menu.Item key="case-view"><Link to="/case-view"/>Cases</Menu.Item>
+            <Menu.Item key="calandar"><Link to="/calendar" />Calendar</Menu.Item>
+            <Menu.Item key="resources"><Link to="/resources" />Resources</Menu.Item>
+            <Menu.Item key="services"><Link to="/services" />Services</Menu.Item>
         </Menu>
     )
 };
