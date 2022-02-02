1a6abfda28f04e81745a73ff9cbd7c9195439c4c
GenHero <51133244+GenHero@users.noreply.github.com>
2022-02-01 19:08:24 -0500

    added semicolon to line 17 in navigation.js

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -11,10 +11,10 @@ function Navigation() {
             <Menu.Item key="home"><Link to ="/"/>Home</Menu.Item>
             <Menu.Item key="case-view"><Link to="/case-view"/>Cases</Menu.Item>
             <Menu.Item key="calandar"><Link to="/calendar" />Calendar</Menu.Item>
-            <Menu.Item key="resources"><Link to="/resources" />Resources</Menu.Item>
+            <Menu.Item key="resources"><Link to="/resources" />Staff Resources</Menu.Item>
             <Menu.Item key="services"><Link to="/services" />Services</Menu.Item>
         </Menu>
-    )
+    );
 };
 
 export default Navigation;
