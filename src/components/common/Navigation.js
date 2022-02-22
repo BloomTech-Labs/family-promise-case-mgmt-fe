4ca4d47c8dcdc03603a6f71844d5d4cd5d2d8dd4
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-02-22 13:21:49 -0500

    clicking `Cases` on navbar route to `/cases`

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -4,17 +4,36 @@ import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
-    const [current, setCurrent] = useState(null);
-    
-    return(
-        <Menu onClick={({key}) => setCurrent(key)} selectedKeys={[current]} mode="horizontal">
-            <Menu.Item key="home"><Link to ="/"/>Home</Menu.Item>
-            <Menu.Item key="case-view"><Link to="/case-view"/>Cases</Menu.Item>
-            <Menu.Item key="calandar"><Link to="/calendar" />Calendar</Menu.Item>
-            <Menu.Item key="resources"><Link to="/resources" />Staff Resources</Menu.Item>
-            <Menu.Item key="services"><Link to="/services" />Services</Menu.Item>
-        </Menu>
-    );
-};
+  const [current, setCurrent] = useState(null);
+
+  return (
+    <Menu
+      onClick={({ key }) => setCurrent(key)}
+      selectedKeys={[current]}
+      mode="horizontal"
+    >
+      <Menu.Item key="home">
+        <Link to="/" />
+        Home
+      </Menu.Item>
+      <Menu.Item key="cases">
+        <Link to="/cases" />
+        Cases
+      </Menu.Item>
+      <Menu.Item key="calandar">
+        <Link to="/calendar" />
+        Calendar
+      </Menu.Item>
+      <Menu.Item key="resources">
+        <Link to="/resources" />
+        Staff Resources
+      </Menu.Item>
+      <Menu.Item key="services">
+        <Link to="/services" />
+        Services
+      </Menu.Item>
+    </Menu>
+  );
+}
 
 export default Navigation;
