1f7d26f1e7791ac6e0daa9039f47d8dd34e51138
stkirk <shaanekirk@gmail.com>
2022-03-04 14:29:22 -0500

    added useEffect for axios call later, added sub panel to display all notes

--- src/components/common/CaseDetailsCollapse/CaseDetailsCollapse.js
+++ src/components/common/CaseDetailsCollapse/CaseDetailsCollapse.js
@@ -9,16 +9,27 @@ const CaseDetailsCollapse = () => {
   }
 
   const text = `
-    A dog is a type of domesticated animal.
-    Known for its loyalty and faithfulness,
-    it can be found as a welcome guest in many households across the world.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   `;
+
+  useEffect(() => {
+    // will need axios get request to receive case details state data from backend
+    console.log('useEffect fired!');
+  }, []);
+
   return (
     <div>
       <h1>Case Details Collapse</h1>
-      <Collapse defaultActiveKey={['clientNotes']} onChange={callback}>
-        <Panel header="Client notes" key="clientNotes">
-          <p>{text}</p>
+      <Collapse defaultActiveKey={['notesPreview']} onChange={callback}>
+        <Panel header="Client Notes" key="notesPreview">
+          <p>display most recent note, this panel is default active</p>
+          <Collapse onChange={callback}>
+            <Panel header="Show All Notes" key="clientNotes">
+              <p>note 2 visible in secondary exapndable panel</p>
+              <p>note 3 visible in secondary exapndable panel</p>
+              <p>note 4 visible in secondary exapndable panel</p>
+            </Panel>
+          </Collapse>
         </Panel>
         <Panel header="Client/Family Information" key="clientFamilyInformation">
           <p>{text}</p>
