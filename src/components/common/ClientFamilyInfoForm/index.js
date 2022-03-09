28d04a383cd5333ba4127412a68c789c5ae3c69b
Chevy <75184977+chavionjackson@users.noreply.github.com>
2022-03-09 15:30:23 -0800

    created a portion of client/family inputs on form

--- src/components/common/ClientFamilyInfoForm/index.js
+++ src/components/common/ClientFamilyInfoForm/index.js
@@ -1,11 +1,264 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Menu,
+  Form,
+  Input,
+  Button,
+  Select,
+  InputNumber,
+  DatePicker,
+  Checkbox,
+  Dropdown,
+} from 'antd';
+import axios from 'axios';
 
-function ClientFamilyInfoForm() {
+const ClientFamilyInfoForm = () => {
+  const { TextArea } = Input;
+
+  const dateFormat = 'MM/DD/YYYY';
+
+  // Initial Empty Values for form
+  const clientInput = {
+    first_name: '',
+    last_name: '',
+    head_of_household: '',
+    family_members: '',
+    ssn: null,
+    ethnicity: '',
+    race: '',
+    gender: '',
+    self_describe: '',
+    sexual_orientation: '',
+    other: '',
+    homeless_history: '',
+    employment: '',
+  };
+
+  const [clientInfo, setClientInfo] = useState(clientInput);
+
+  // Change Handler used for all fields except Select.
+  const formChanges = event => {
+    const { name, value } = event.target;
+    setClientInfo({ ...clientInfo, [name]: value });
+  };
+  // antd's answer to onSubmit
+  const onFinish = () => {
+    // The endpoint used is a placeholder. The response will always be the submitted data.
+    axios
+      .post('https://jsonplaceholder.typicode.com/posts', {
+        title: 'Mock POST',
+        body: clientInfo,
+      })
+      .then(res => {
+        alert('Sucess! Data submitted:\n' + JSON.stringify(res.data.body));
+      })
+      .catch(() => {
+        console.error('Something Went Wrong');
+      });
+  };
+
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Client and Family Information</h1>
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        onFinish={onFinish}
+        style={{ paddingTop: '2rem' }}
+      >
+        <Form.Item label="Initial Date" onChange={formChanges}>
+          <DatePicker format={dateFormat} />
+        </Form.Item>
+        <Form.Item
+          label="First Name"
+          value={clientInfo.first_name}
+          onChange={formChanges}
+        >
+          <Input name="First Name" placeholder="First Name" />
+        </Form.Item>
+        <Form.Item
+          label="Last Name"
+          value={clientInfo.last_name}
+          onChange={formChanges}
+        >
+          <Input name="Last Name" placeholder="Last Name" />
+        </Form.Item>
+        <Form.Item label="SSN" value={clientInfo.ssn} onChange={formChanges}>
+          <InputNumber name="SSN" />
+        </Form.Item>
+        <Form.Item label="DOB" onChange={formChanges}>
+          <DatePicker format={dateFormat} />
+        </Form.Item>
+        <Form.Item
+          label="Ethnicity"
+          value={clientInfo.ethnicity}
+          onChange={formChanges}
+        >
+          <Select
+            value={clientInfo.ethnicity}
+            placeholder="ethnicity"
+            name="ethnicity"
+            onChange={value => {
+              setClientInfo({ ...clientInfo, ethnicity: value });
+            }}
+          >
+            <Select.Option value="American Indian or Alaska Native" />
+            <Select.Option value="Asian" />
+            <Select.Option value="Black or African American" />
+            <Select.Option value="Native Hawaiian or Other Pacific Islander" />
+            <Select.Option value="White or Caucasian" />
+            <Select.Option value="Hispanic or Latino or Spanish Origin" />
+            <Select.Option value="Non-Resident Alien (of any race or ethnicity)" />
+            <Select.Option value="Multiracial or Biracial" />
+          </Select>
+        </Form.Item>
+        <Form.Item label="Race" value={clientInfo.race} onChange={formChanges}>
+          <Select
+            value={clientInfo.race}
+            placeholder="race"
+            name="race"
+            onChange={value => {
+              setClientInfo({ ...clientInfo, race: value });
+            }}
+          >
+            <Select.Option value="American Indian or Alaska Native" />
+            <Select.Option value="Asian" />
+            <Select.Option value="Black or African American" />
+            <Select.Option value="Native Hawaiian or Other Pacific Islander" />
+            <Select.Option value="White" />
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Gender"
+          value={clientInfo.gender}
+          onChange={formChanges}
+        >
+          <Select
+            value={clientInfo.gender}
+            placeholder="gender"
+            name="gender"
+            onChange={value => {
+              setClientInfo({ ...clientInfo, gender: value });
+            }}
+          >
+            <Select.Option value="Male" />
+            <Select.Option value="Female" />
+            <Select.Option value="Transgender Male" />
+            <Select.Option value="Transgender Female" />
+            <Select.Option value="Non-Binary" />
+            <Select.Option value="Prefer to Self-Describe" />
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Self-Describe"
+          value={clientInfo.self_describe}
+          onChange={formChanges}
+        >
+          <Input name="self-describe" placeholder="Self-Describe" />
+        </Form.Item>
+        <Form.Item
+          label="Sexual Orientation"
+          value={clientInfo.sexual_orientation}
+          onChange={formChanges}
+        >
+          <Select
+            value={clientInfo.sexual_orientation}
+            placeholder="Sexual Orientation"
+            name="sexual Orientation"
+            onChange={value => {
+              setClientInfo({ ...clientInfo, sexual_orientation: value });
+            }}
+          >
+            <Select.Option value="Asexual" />
+            <Select.Option value="Bisexual" />
+            <Select.Option value="Gay" />
+            <Select.Option value="Lesbian" />
+            <Select.Option value="Heterosexual/Straight" />
+            <Select.Option value="Pansexual" />
+            <Select.Option value="Pansexual" />
+            <Select.Option value="Queer" />
+            <Select.Option value="Preferred Not to Answer" />
+            <Select.Option value="Other" />
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          label="If, Other"
+          value={clientInfo.other}
+          onChange={formChanges}
+        >
+          <Input name="other" placeholder="Other" />
+        </Form.Item>
+
+        <Form.Item
+          label="Name of Family Members"
+          value={clientInfo.family_members}
+          onChange={formChanges}
+        >
+          <TextArea
+            rows={4}
+            name="family_members"
+            placeholder="Name of Family Members"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Education Status"
+          value={clientInfo.education_level}
+          onChange={formChanges}
+        >
+          <Select
+            value={clientInfo.education_level}
+            placeholder="Education Status"
+            name="education_level"
+            onChange={value => {
+              setClientInfo({ ...clientInfo, education_level: value });
+            }}
+          >
+            <Select.Option value="High School Graduate" />
+            <Select.Option value="Some College" />
+            <Select.Option value="College Graduate" />
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Prior History"
+          value={clientInfo.homeless_history}
+          onChange={formChanges}
+        >
+          <TextArea
+            rows={4}
+            name="homeless_history"
+            placeholder="Prior History/Prior Episodes of Homelessness"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Employment Status"
+          value={clientInfo.employment}
+          onChange={formChanges}
+        >
+          <Select
+            name="employment"
+            onChange={value => {
+              setClientInfo({ ...clientInfo, employment: value });
+            }}
+          >
+            <Select.Option value="Employed" />
+            <Select.Option value="Unemployed" />
+          </Select>
+        </Form.Item>
+        <Form.Item>
+          <Button
+            htmlType="submit"
+            style={{
+              marginLeft: '30%',
+            }}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
-}
+};
 
 export default ClientFamilyInfoForm;
