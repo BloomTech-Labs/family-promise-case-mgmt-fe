e7d6122258f94d41f6e4ba12062861a45bc7f54d
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-03-10 19:08:37 -0500

    created cases page

--- src/components/pages/CaseView/CaseViewContainer.js
+++ src/components/pages/CaseView/CaseViewContainer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import CaseDashboard from './Dashboard';
 import FamilyMembersTable from './FamilyTable';
 
-const CaseViewPage = () => {
+const CaseView = () => {
   const family = {
     householdId: 123456789,
     active: true,
     familySize: 4,
-    history: "https://www.google.com", 
-    documents: "https://www.google.com", 
-    applications: "https://www.google.com", 
+    history: 'https://www.google.com',
+    documents: 'https://www.google.com',
+    applications: 'https://www.google.com',
     headOfHousehold: {
       firstname: 'John',
       lastname: 'Smith',
@@ -19,53 +19,53 @@ const CaseViewPage = () => {
     },
     familyMembers: [
       {
-            id: '1',
-            firstname: 'John',
-            lastname: 'Brown',
-            birthDate: '12-5-1498',
-            gender: 'Male',
-            relationshipToGuardian: "Guardian",
-            ssn: '123-45-678',
-            veteran: "No",
-            education: "Some High School",
-            employment: "Employed"
-          },
-          {
-            id: '2',
-            firstname: 'Jim', 
-            lastname: 'Green',
-            birthDate: '5-3-8965',
-            gender: 'Male',
-            relationshipToGuardian: "Son",
-            ssn: '123-45-678',
-            veteran: "No",
-            education: "Elementary",
-            employment: "Student"
-          },
-          {
-            id: '3',
-            firstname: 'Jen', 
-            lastname: 'Black',
-            birthDate: '11-5-7894',
-            gender: 'Female',
-            relationshipToGuardian: "Wife",
-            ssn: '123-45-678',
-            veteran: "No",
-            education: "High School",
-            employment: "Employed"
-          },
-          {
-            id: '4',
-            firstname: 'Jim', 
-            lastname: 'Red',
-            birthDate: '63-75-4826',
-            gender: 'Male',
-            relationshipToGuardian: "Father",
-            ssn: '123-45-678',
-            veteran: "No",
-            education: "Some High School",
-            employment: "Retired"
-            },
+        id: '1',
+        firstname: 'John',
+        lastname: 'Brown',
+        birthDate: '12-5-1498',
+        gender: 'Male',
+        relationshipToGuardian: 'Guardian',
+        ssn: '123-45-678',
+        veteran: 'No',
+        education: 'Some High School',
+        employment: 'Employed',
+      },
+      {
+        id: '2',
+        firstname: 'Jim',
+        lastname: 'Green',
+        birthDate: '5-3-8965',
+        gender: 'Male',
+        relationshipToGuardian: 'Son',
+        ssn: '123-45-678',
+        veteran: 'No',
+        education: 'Elementary',
+        employment: 'Student',
+      },
+      {
+        id: '3',
+        firstname: 'Jen',
+        lastname: 'Black',
+        birthDate: '11-5-7894',
+        gender: 'Female',
+        relationshipToGuardian: 'Wife',
+        ssn: '123-45-678',
+        veteran: 'No',
+        education: 'High School',
+        employment: 'Employed',
+      },
+      {
+        id: '4',
+        firstname: 'Jim',
+        lastname: 'Red',
+        birthDate: '63-75-4826',
+        gender: 'Male',
+        relationshipToGuardian: 'Father',
+        ssn: '123-45-678',
+        veteran: 'No',
+        education: 'Some High School',
+        employment: 'Retired',
+      },
     ],
   };
 
@@ -76,10 +76,10 @@ const CaseViewPage = () => {
         {family.headOfHousehold.lastname}'s family (household ID:{' '}
         {family.householdId})
       </h1>
-      <CaseDashboard familyData ={family}/>
-      <FamilyMembersTable familyData ={family}/>
+      <CaseDashboard familyData={family} />
+      <FamilyMembersTable familyData={family} />
     </div>
   );
 };
 
-export default CaseViewPage;
+export default CaseView;
---
+++ src/components/pages/Cases/CasesContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CasesContainer = () => {
+  return <>CasesContainer</>;
+};
+
+export default CasesContainer;
