2cf8209a0568f63e01f8c8946f2d4d34ff7e47a7
Steve Clair <steventclair@gmail.com>
2022-03-21 22:34:04 -0400

    Bug Fix: Navbar Functionality Menu Items (#27)
    * swapped current link items to correct configuration based on the wireframe
    
    * added addtional links to navbar
    
    * added links to their respective pages
    
    * Home link removed from navbar
    
    * added key params to all of the menu item tags.

--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -19,21 +19,29 @@ const NavHeader = () => {
         mode="horizontal"
         defaultSelectedKeys={['2']}
       >
-        <Menu.Item key="home">
-          <Link to="/" />
-          Home
-        </Menu.Item>
-        <Menu.Item key="cases">
-          <Link to="/cases" />
-          Cases
+        <Menu.Item key="dashboard">
+          <Link to="dashboard" />
+          Dashboard
         </Menu.Item>
         <Menu.Item key="calandar">
           <Link to="/calendar" />
           Calendar
         </Menu.Item>
+        <Menu.Item key="cases">
+          <Link to="/cases" />
+          Cases
+        </Menu.Item>
+        <Menu.Item key="resources">
+          <Link to="resources" />
+          Resources
+        </Menu.Item>
         <Menu.Item key="services">
           <Link to="/services" />
           Services
+        </Menu.Item>
+        <Menu.Item key="client-intake-form">
+          <Link to="client-intake-form" />
+          Intake
         </Menu.Item>
         <Menu.Item
           key="logout"
