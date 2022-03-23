27feaa69af51256172989930ee96cbf2a0a51f3c
Rex-1031 <steventclair@gmail.com>
2022-03-22 22:27:15 -0400

    form items added to ClientFamilyIntake

--- src/components/common/ClientIntakeForm/IntakeFormSections/ClientFamilyIntake.js
+++ src/components/common/ClientIntakeForm/IntakeFormSections/ClientFamilyIntake.js
@@ -1,7 +1,122 @@
+import { DatePicker, Form, Input, Select } from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
 import React from 'react';
 
 const ClientFamilyIntake = () => {
-  return <div>ClientFamilyIntake</div>;
+  const [form] = Form.useForm();
+
+  const inputStyles = {
+    width: '20rem',
+  };
+
+  const inputStylesShort = {
+    width: '7rem',
+  };
+  return (
+    <Form form={form} layout="vertical">
+      <Form.Item label="Inital Intake Date">
+        <DatePicker format="MM/DD/YYYY" />
+      </Form.Item>
+
+      <h2>Head of Household</h2>
+      <div className="hohContainer">
+        <Form.Item label="First Name">
+          <Input placeholder="First Name" style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="Last Name">
+          <Input placeholder="Last Name" style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="SSN">
+          <Input placeholder="XXX-XX-XXXX" style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="DOB">
+          <DatePicker format="MM/DD/YYYY" />
+        </Form.Item>
+
+        <Form.Item label="Ethnicity">
+          <Select style={inputStyles} placeholder="-- Select --">
+            <Select.Option value="American Indian or Alaska Native" />
+            <Select.Option value="Asian" />
+            <Select.Option value="Black or African American" />
+            <Select.Option value="Native Hawaiian or Other Pacific Islander" />
+            <Select.Option value="White or Caucasian" />
+            <Select.Option value="Hispanic or Latino or Spanish Origin" />
+            <Select.Option value="Non-Resident Alien (of any race or ethnicity)" />
+            <Select.Option value="Multiracial or Biracial" />
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Race">
+          <Select style={inputStyles} placeholder="-- Select --">
+            <Select.Option value="American Indian or Alaska Native" />
+            <Select.Option value="Asian" />
+            <Select.Option value="Black or African American" />
+            <Select.Option value="Native Hawaiian or Other Pacific Islander" />
+            <Select.Option value="White" />
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Gender">
+          <Select style={inputStyles} placeholder="-- Select --">
+            <Select.Option value="Male" />
+            <Select.Option value="Female" />
+            <Select.Option value="Transgender Male" />
+            <Select.Option value="Transgender Female" />
+            <Select.Option value="Non-Binary" />
+            <Select.Option value="Prefer to Self-Describe" />
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Self-Describe">
+          <TextArea placeholder="Self-Describe" style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="Sexual Orientation">
+          <Select style={inputStyles} placeholder="-- Select --">
+            <Select.Option value="Asexual" />
+            <Select.Option value="Bisexual" />
+            <Select.Option value="Gay" />
+            <Select.Option value="Lesbian" />
+            <Select.Option value="Heterosexual/Straight" />
+            <Select.Option value="Pansexual" />
+            <Select.Option value="Pansexual" />
+            <Select.Option value="Queer" />
+            <Select.Option value="Preferred Not to Answer" />
+            <Select.Option value="Other" />
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="If Other">
+          <TextArea placeholder="Other" style={inputStyles} />
+        </Form.Item>
+      </div>
+      <h2>Last Known Address</h2>
+      <div className="addressContainer">
+        <Form.Item label="Address 1">
+          <Input placeholder="123 Somewhere Rd." style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="Address 2">
+          <Input placeholder="Apt. #" style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="City">
+          <Input placeholder="City Name" style={inputStyles} />
+        </Form.Item>
+
+        <Form.Item label="State">
+          <Input placeholder="State Abbrv." style={inputStylesShort} />
+        </Form.Item>
+
+        <Form.Item label="Zip">
+          <Input placeholder="Zip Code" style={inputStylesShort} />
+        </Form.Item>
+      </div>
+    </Form>
+  );
 };
 
 export default ClientFamilyIntake;
--- src/components/common/ClientIntakeForm/index.js
+++ src/components/common/ClientIntakeForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Input, Button, Select, InputNumber } from 'antd';
 import axios from 'axios';
+import ClientFamilyIntake from './IntakeFormSections/ClientFamilyIntake';
 
 // the structure of this component is supposed to mirror that of ClientListTable, in which we have a folder with a file index.js as the default export containing all other components within. When you import a folder and it has index.js, you don't need to specify a file as index.js is the default. This allows us to group a main component and its subcomponents in a single folder that can be seamlessly imported.
 
@@ -42,94 +43,94 @@ export const ClientIntake = () => {
   };
 
   return (
-    <div>
-      <Form
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-        onFinish={onFinish}
-        style={{ paddingTop: '2rem' }}
-      >
-        <Form.Item
-          label="Head of Household"
-          value={clientInfo.head_of_household}
-          onChange={formChanges}
-        >
-          <Input name="head_of_household" placeholder="Head of Household" />
-        </Form.Item>
-        <Form.Item
-          label="Name of Family Members"
-          value={clientInfo.family_members}
-          onChange={formChanges}
-        >
-          <TextArea
-            rows={4}
-            name="family_members"
-            placeholder="Name of Family Members"
-          />
-        </Form.Item>
-        <Form.Item
-          label="Family Size"
-          value={clientInfo.family_size}
-          onChange={formChanges}
-        >
-          <InputNumber name="family_size" />
-        </Form.Item>
-        <Form.Item
-          label="Education Status"
-          value={clientInfo.education_level}
-          onChange={formChanges}
-        >
-          <Select
-            value={clientInfo.education_level}
-            placeholder="Education Status"
-            name="education_level"
-            onChange={value => {
-              setClientInfo({ ...clientInfo, education_level: value });
-            }}
-          >
-            <Select.Option value="High School Graduate" />
-            <Select.Option value="Some College" />
-            <Select.Option value="College Graduate" />
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="Prior History"
-          value={clientInfo.homeless_history}
-          onChange={formChanges}
-        >
-          <TextArea
-            rows={4}
-            name="homeless_history"
-            placeholder="Prior History/Prior Episodes of Homelessness"
-          />
-        </Form.Item>
-        <Form.Item
-          label="Employment Status"
-          value={clientInfo.employment}
-          onChange={formChanges}
-        >
-          <Select
-            name="employment"
-            onChange={value => {
-              setClientInfo({ ...clientInfo, employment: value });
-            }}
-          >
-            <Select.Option value="Employed" />
-            <Select.Option value="Unemployed" />
-          </Select>
-        </Form.Item>
-        <Form.Item>
-          <Button
-            htmlType="submit"
-            style={{
-              marginLeft: '30%',
-            }}
-          >
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+    // <div>
+    //   <Form
+    //     labelCol={{ span: 4 }}
+    //     wrapperCol={{ span: 14 }}
+    //     layout="horizontal"
+    //     onFinish={onFinish}
+    //     style={{ paddingTop: '2rem' }}
+    //   >
+    //     <Form.Item
+    //       label="Head of Household"
+    //       value={clientInfo.head_of_household}
+    //       onChange={formChanges}
+    //     >
+    //       <Input name="head_of_household" placeholder="Head of Household" />
+    //     </Form.Item>
+    //     <Form.Item
+    //       label="Name of Family Members"
+    //       value={clientInfo.family_members}
+    //       onChange={formChanges}
+    //     >
+    //       <TextArea
+    //         rows={4}
+    //         name="family_members"
+    //         placeholder="Name of Family Members"
+    //       />
+    //     </Form.Item>
+    //     <Form.Item
+    //       label="Family Size"
+    //       value={clientInfo.family_size}
+    //       onChange={formChanges}
+    //     >
+    //       <InputNumber name="family_size" />
+    //     </Form.Item>
+    //     <Form.Item
+    //       label="Education Status"
+    //       value={clientInfo.education_level}
+    //       onChange={formChanges}
+    //     >
+    //       <Select
+    //         value={clientInfo.education_level}
+    //         placeholder="Education Status"
+    //         name="education_level"
+    //         onChange={value => {
+    //           setClientInfo({ ...clientInfo, education_level: value });
+    //         }}
+    //       >
+    //         <Select.Option value="High School Graduate" />
+    //         <Select.Option value="Some College" />
+    //         <Select.Option value="College Graduate" />
+    //       </Select>
+    //     </Form.Item>
+    //     <Form.Item
+    //       label="Prior History"
+    //       value={clientInfo.homeless_history}
+    //       onChange={formChanges}
+    //     >
+    //       <TextArea
+    //         rows={4}
+    //         name="homeless_history"
+    //         placeholder="Prior History/Prior Episodes of Homelessness"
+    //       />
+    //     </Form.Item>
+    //     <Form.Item
+    //       label="Employment Status"
+    //       value={clientInfo.employment}
+    //       onChange={formChanges}
+    //     >
+    //       <Select
+    //         name="employment"
+    //         onChange={value => {
+    //           setClientInfo({ ...clientInfo, employment: value });
+    //         }}
+    //       >
+    //         <Select.Option value="Employed" />
+    //         <Select.Option value="Unemployed" />
+    //       </Select>
+    //     </Form.Item>
+    //     <Form.Item>
+    //       <Button
+    //         htmlType="submit"
+    //         style={{
+    //           marginLeft: '30%',
+    //         }}
+    //       >
+    //         Submit
+    //       </Button>
+    //     </Form.Item>
+    //   </Form>
+    <ClientFamilyIntake />
   );
 };
