8ef4c49a7b35a1c496cd0add553a603825de399d
Marina Monsivais <minamonsi@me.com>
2022-05-02 19:35:01 -0500

    Renamed Header.js and edited the names in the corresponding files

--- src/components/pages/Dashboard/RenderDashboard.js
+++ src/components/pages/Dashboard/RenderDashboard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Header } from '../../common/Header';
+import { DashHeader } from '../../common/DashHeader';
 import CaseTable from '../../common/CaseTable';
 import RecentActivity from '../../common/RecentActivity';
 
 function RenderDashboard() {
   return (
     <div>
-      <Header />
+      <DashHeader />
       <CaseTable />
       <RecentActivity />
     </div>
