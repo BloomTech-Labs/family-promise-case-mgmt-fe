51e8976725810ab1e73634f3f8aed48fdc88e7fe
Mallory <mallorydoescoding@gmail.com>
2022-05-06 10:59:43 -0500

    commiting so I can rebase.

--- src/components/common/CaseDetailsCollapse/CaseDetailsCollapse.js
+++ src/components/common/CaseDetailsCollapse/CaseDetailsCollapse.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { Button, Collapse } from 'antd';
-import ClientFamilyInfoForm from '../CaseDetailsForms/ClientFamilyInformation';
-
-import { HouseholdInformationForm } from '../CaseDetailsForms/HouseholdInformationForm';
-import { initialFormValues, Notes } from '../CaseDetailsForms/Notes';
-import SingleNote from '../CaseDetailsForms/SingleNote';
 
 const { Panel } = Collapse;
 
@@ -41,52 +36,6 @@ const CaseDetailsCollapse = () => {
   return (
     <div className="CaseDetailsCollapse">
       {/* change h1 text to read appropriate API data when availible */}
-      <h1>Case Details</h1>
-      <Collapse defaultActiveKey={['notesPreview']} onChange={callback}>
-        <Panel
-          header="Client Notes"
-          key="notesPreview"
-          style={{ margin: 'auto' }}
-          extra={genAddNoteButton()}
-        >
-          <SingleNote props={initialFormValues} />
-          <Collapse onChange={callback}>
-            <Panel header="Show All Notes" key="showAllNotes">
-              <Notes />
-            </Panel>
-          </Collapse>
-        </Panel>
-        <Panel header="Client/Family Information" key="clientFamilyInformation">
-          <ClientFamilyInfoForm />
-        </Panel>
-        <Panel header="Household Information" key="householdInformation">
-          <HouseholdInformationForm />
-        </Panel>
-        <Panel header="Education" key="education">
-          {/* insert Education Form here and delete p tag w/ dummy text */}
-          <p>{text}</p>
-        </Panel>
-        <Panel header="Employment" key="employment">
-          {/* insert Employment Form here and delete p tag w/ dummy text */}
-          <p>{text}</p>
-        </Panel>
-        <Panel header="Finances" key="finances">
-          {/* insert Finances Form here and delete p tag w/ dummy text */}
-          <p>{text}</p>
-        </Panel>
-        <Panel header="Insurance" key="insurance">
-          {/* insert Insurance Form here and delete p tag w/ dummy text */}
-          <p>{text}</p>
-        </Panel>
-        <Panel header="Documents" key="documents">
-          {/* insert Documents Form here and delete p tag w/ dummy text */}
-          <p>{text}</p>
-        </Panel>
-        <Panel header="Goals/Strengths/Obstacles" key="goalsStrengthsObstacles">
-          {/* insert Goals/Strengths/Obstacles Form here and delete p tag w/ dummy text */}
-          <p>{text}</p>
-        </Panel>
-      </Collapse>
     </div>
   );
 };
