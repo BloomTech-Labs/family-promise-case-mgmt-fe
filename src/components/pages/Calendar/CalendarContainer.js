ca6d19c79b8f77525a524a2509d107a81649a2d1
Valeriusdev <kakao757@gmail.com>
2022-06-01 02:18:53 -0700

    set up CalendarContainer.js file

---
+++ src/components/pages/Calendar/CalendarContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RenderCalendar from './RenderCalendar';
+
+const CalendarContainer = () => {
+  return (
+    <>
+      <RenderCalendar />
+    </>
+  );
+};
+
+export default CalendarContainer;
