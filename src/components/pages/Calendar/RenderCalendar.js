8fac285eea2936fbdf71e1baafa8c33c2def6891
Valeriusdev <kakao757@gmail.com>
2022-06-13 02:28:06 -0700

    wip

--- src/components/pages/Calendar/RenderCalendar.js
+++ src/components/pages/Calendar/RenderCalendar.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { DashHeader } from '../../common/DashHeader';
+import DashHeader from '../../common/DashHeader';
 import NavHeader from '../../common/NavHeader';
-import { Layout } from 'antd';
+import Button from '../../common/Button';
+import { Layout, Card } from 'antd';
 
-const { Header, Footer } = Layout;
+const { Sider, Content, Footer } = Layout;
 
 function RenderCalendar() {
   return (
     <div>
-      <Layout>
-        <Header>
-          <NavHeader />
-        </Header>
+      <Layout className="Layout">
+        <DashHeader />
+        <Layout>
+          <Sider>
+            <Button />
+            This is Sider
+          </Sider>
+          <Content>This is Content</Content>
+        </Layout>
+
+        <Content className="Content">
+          <div className="Content__Container">
+            <Card className="Content__Container-Card">
+              <Button />
+              Hello
+            </Card>
+          </div>
+        </Content>
+        <Footer className="Footer">
+          Family Promise of Spokane ©2022 Created by BloomTech Labs
+        </Footer>
       </Layout>
-      <Footer className="Footer">
-        Family Promise of Spokane ©2022 Created by BloomTech Labs
-      </Footer>
     </div>
   );
 }
