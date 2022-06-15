20afc778146cb4a2799c27e681c8addd1e1b70ca
Marina Monsivais <minamonsi@me.com>
2022-06-15 00:02:50 -0500

    Added button to the card-header and styled it to reflect the UX design

--- src/components/common/RecentActivity.js
+++ src/components/common/RecentActivity.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Avatar, Card } from 'antd';
+import { Avatar, Card, Button } from 'antd';
 import Vector from '../../assets/Vector.png';
 
 const { Meta } = Card;
 
 const RecentActivity = () => {
   return (
-    <>
+    <div className="CardDiv">
       <Card
         style={{
           width: 300,
         }}
       >
+        <div className="CardHeader">
+          <p>
+            First Lastname
+            <br />
+            XXXX_XX_XX{' '}
+          </p>
+          <img className="VectorArrow" src={Vector} alt="arrow" />
+          <Button type="primary">View Case</Button>
+        </div>
+
         <Meta
           avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-          title="First Lastname"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo"
         />
       </Card>
-    </>
+    </div>
   );
 };
 
