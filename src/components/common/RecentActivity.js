314191f287ea469e59053dec6a3e54ec74a12b0c
Marina Monsivais <minamonsi@me.com>
2022-06-14 21:45:00 -0500

    Imported antd card and avatar components to RecentCases.js file, added styling in styles.css file

--- src/components/common/RecentActivity.js
+++ src/components/common/RecentActivity.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Avatar, Card } from 'antd';
+import Vector from '../../assets/Vector.png';
+
+const { Meta } = Card;
 
 const RecentActivity = () => {
-  return <></>;
+  return (
+    <>
+      <Card
+        style={{
+          width: 300,
+        }}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="First Lastname"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo"
+        />
+      </Card>
+    </>
+  );
 };
 
 export default RecentActivity;
