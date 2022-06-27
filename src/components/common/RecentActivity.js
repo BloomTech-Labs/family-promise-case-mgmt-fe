ca6e798a47771cccc33dede2ba694eabe1b68bae
Marina Monsivais <68879755+MinaMonsi@users.noreply.github.com>
2022-06-27 18:26:42 -0500

    Feature/RecentCases  (#48)
    * Created a RecentCases folder, RecentCases.js, RecentCasesContainer.js, and index.js
    
    * Created a RecentCases folder, RecentCases.js, RecentCasesContainer.js, and index.js
    
    * Added a table with some dummy data to display in the component
    
    * Added an h1 tag in RecentCases.js
    
    * Added more css styling to reflect the dark mode figma design
    
    * Added styling to the RecentCases component
    
    * Added another div, H1, and png for the Recent Activity component
    
    * Imported antd card and avatar components to RecentCases.js file, added styling in styles.css file
    
    * Imported RecentActivity component into the RecentCases.js file
    
    * Added button to the card-header and styled it to reflect the UX design
    
    * Edited code in RecentActivity.js
    
    * Added 10 cards to the Recent Activity Component and arranged the cards into columns in styles.css
    
    * Styled the RecentCases div and the RecentActivity div so that the components look even on the page
    
    * Deleted a console.log in RecentActivity.js and changed the RecentCases.js file name
    
    * Removed comments from the styles.css file
    
    * Deleted extra spase from css file
    
    * Added a required space in line 451 of the styles.css file
    
    * Erased extra semicolon
    
    * Corrected a few more formatting errors

--- src/components/common/RecentActivity.js
+++ src/components/common/RecentActivity.js
@@ -1,7 +1,118 @@
 import React from 'react';
+import { Avatar, Card, Button } from 'antd';
+import Vector from '../../assets/Vector.png';
+
+const { Meta } = Card;
+
+const recentCases = [
+  {
+    firstName: 'Frank',
+    lastName: 'Benavides',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Laura',
+    lastName: 'Robago',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Sally',
+    lastName: 'Smith',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Jen',
+    lastName: 'Sanders',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Salvador',
+    lastName: 'Pena',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Susy Que',
+    lastName: '',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Thanasis',
+    lastName: 'Lindholm',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Bryan',
+    lastName: 'Torwalt',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Felipe',
+    lastName: 'Venczel',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+  {
+    firstName: 'Iona',
+    lastName: 'Ermacora',
+    date: ' XXXX_XX_XX',
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    note:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pulvinar sollicitudin consectetur massa faucibus commodo',
+  },
+];
 
 const RecentActivity = () => {
-  return <></>;
+  return (
+    <div className="CardDiv">
+      {recentCases.map(recentCase => (
+        <Card
+          style={{
+            width: 300,
+          }}
+        >
+          <div className="CardHeader">
+            <p>
+              {recentCase.firstName} {recentCase.lastName}
+              <br />
+            </p>
+            <img className="VectorArrow" src={Vector} alt="arrow" />
+            <Button type="primary">View Case</Button>
+          </div>
+
+          <Meta
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+            description={recentCase.note}
+          />
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default RecentActivity;
