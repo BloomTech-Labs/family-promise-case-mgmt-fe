90456cc39cb3aee67769a15d5006c214f1ce2214
JordonP123 <jordonaren@gmail.com>
2022-11-04 00:02:37 -0400

    hit the /:id/notes endpoint, mapped out notes in divs

--- src/components/common/CaseDetails/printNotes.js
+++ src/components/common/CaseDetails/printNotes.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const PrintNotes = () => {
-  return <>Hello World!</>;
+  const [notes, setNotes] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/clients/2/notes')
+      .then(res => {
+        setNotes(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <>
+      {notes.length > 1
+        ? notes.map(note => <div key={note.id}>{note.message}</div>)
+        : 'No notes for this client yet'}
+    </>
+  );
 };
 
 export default PrintNotes;
