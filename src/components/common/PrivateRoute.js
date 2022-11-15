16653f54bcc91eb333eb74afe757671ca43fd0a5
JordonP123 <jordonaren@gmail.com>
2022-11-14 22:21:06 -0500

    removed all okta, implemented basic login with a temp landing page

---
+++ src/components/common/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
+
+const PrivateRoute = ({ component, ...args }) => (
+  <Route
+    component={withAuthenticationRequired(component, {
+      onRedirecting: () => 'Loading...',
+    })}
+    {...args}
+  />
+);
+
+export default PrivateRoute;
