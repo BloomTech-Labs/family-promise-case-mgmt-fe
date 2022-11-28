03f146c94fec53bb9518c53e6848f99cb8ad22ca
unknown <ramib1207@gmail.com>
2022-11-28 18:25:53 -0500

    Case-Details view updated to match the figma a bit more

--- src/components/pages/CaseDetails/RenderCaseDetails.js
+++ src/components/pages/CaseDetails/RenderCaseDetails.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { Layout, Card } from 'antd';
 import NavHeader from '../../common/NavHeader';
 import BreadCrumbs from '../../common/BreadCrumbs';
+import DashHeader from '../../common/DashHeader';
 import { CaseDetails } from '../../common/CaseDetails';
 const { Header, Content, Footer } = Layout;
 
@@ -11,22 +12,22 @@ const RenderCaseDetails = () => {
 
   return (
     <div className="RenderLayoutTemplate">
-      <Layout className="Layout">
-        <Header className="Header">
-          <NavHeader />
-        </Header>
-        <Content className="Content">
-          <BreadCrumbs pathname={location.pathname} />
-          <div className="Content__Container">
-            <Card className="Content__Container-Card">
-              <CaseDetails />
-            </Card>
-          </div>
-        </Content>
-        <Footer className="Footer">
-          Family Promise of Spokane ©2022 Created by BloomTech Labs
-        </Footer>
-      </Layout>
+      {/* <Layout className="Layout"> */}
+      {/* <Header className="Header"> */}
+      <DashHeader />
+      {/* </Header> */}
+      <Content className="Content">
+        {/* <BreadCrumbs pathname={location.pathname} /> */}
+        <div className="Content__Container">
+          <Card className="Content__Container-Card">
+            <CaseDetails />
+          </Card>
+        </div>
+      </Content>
+      <Footer className="Footer">
+        Family Promise of Spokane ©2022 Created by BloomTech Labs
+      </Footer>
+      {/* </Layout> */}
     </div>
   );
 };
