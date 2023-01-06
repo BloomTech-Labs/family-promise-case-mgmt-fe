a9e4f100600b3d020ddad55de2e6648fb8273594
Jack Nunnink <jack.nunnink@protonmail.com>
2023-01-06 15:44:46 -0500

    FP-53 landing page testing functional and passing

--- src/__tests__/TempLandingPage.test.js
+++ src/__tests__/TempLandingPage.test.js
@@ -2,16 +2,14 @@ import React from 'react';
 import TempLandingPage from '../components/pages/TempLandingPage/TempLandingPage';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { getByText } from '@testing-library/react';
 
 describe('Components render given route', () => {
   test('<TempLandingPage /> is rendering', () => {
-    render(
+    const { getByTestId } = render(
       <Router>
         <TempLandingPage />
       </Router>
     );
-    const { element } = getByText(/log in/i);
-    expect(element.toBeInTheDocument());
+    expect(getByTestId('loginBtn')).toHaveTextContent(/log in/i);
   });
 });
