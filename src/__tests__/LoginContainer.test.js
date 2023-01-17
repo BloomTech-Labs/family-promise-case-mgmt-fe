1b0a358a1c1f5707e1616af0899ea686b4eb264d
Jack Nunnink <jack.nunnink@protonmail.com>
2023-01-17 12:18:10 -0500

    FP-53 user can log in tests can now select log in button and click it

--- src/__tests__/LoginContainer.test.js
+++ src/__tests__/LoginContainer.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 
 import LoginContainer from '../components/pages/Login/LoginContainer.js';
+import TempLandingPage from '../components/pages/TempLandingPage/TempLandingPage.js';
 
 describe('<LoginContainer /> test suite', () => {
+  const { getByTestId } = render(
+    <Router>
+      <TempLandingPage />
+    </Router>
+  );
+
   test('sign in widget mounts successfully', () => {
-    const { container } = render(<LoginContainer />);
-    expect(container.querySelector('#sign-in-widget')).toBeTruthy();
+    expect(getByTestId('loginBtn')).toHaveTextContent(/log in/i);
+  });
+
+  test('user can log in', async () => {
+    waitFor(() => fireEvent.click(screen.getByText(/log in/i)));
+
+    await waitFor(() => screen.getByText(/doe, john/i));
+
+    expect(screen.getByText(/doe, john/i)).toHaveTextContent('Doe, John');
   });
 });
