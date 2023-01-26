1d69a7e901514aebd535a158e64c96a8ef1ded3a
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-01-26 10:48:49 -0700

    CaseActivity component created

---
+++ src/components/pages/CaseView/CaseActivity.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function CaseActivity() {
+  //TODO:
+  // - Add a Case Logger function taking an action, and the time, then moving that data to the caseActivity
+
+  return (
+    <div>
+      <ol>
+        <li>Case Opened - 9/19/2/23 4:23pm</li>
+        <li>Case Changed - 10/02/23 2:12pm</li>
+        <li>Case Closed - 12/12/23 4:23pm</li>
+      </ol>
+    </div>
+  );
+}
