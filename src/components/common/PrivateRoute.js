7c93d7518143daa910508980a562027da55c5dc5
Sharon Odima <odimasharon@gmail.com>
2023-01-29 15:00:03 -0500

    Sharon Odima updated endpoint from API to Auth0

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -5,29 +5,49 @@ import { connect } from 'react-redux';
 import * as actions from '../../state/actions/authTokenAction';
 import * as actions1 from '../../state/actions/profileAction';
 import { useAuth0 } from '@auth0/auth0-react';
-import { getProfileData } from '../../api/index';
+//import { getProfileData } from '../../api/index';
 
 const PrivateRoute = ({ component, ...args }) => {
   const { getAccessTokenSilently, user, isAuthenticated } = useAuth0();
+  const { setAuthToken, token, setProfile } = args;
 
   useEffect(() => {
-    if (!args.token) {
+    if (!token) {
       getAccessTokenSilently()
         .then(token => {
-          args.setAuthToken(token);
+          setAuthToken(token);
           //you will eventually change this hard-coded 1 in the getProfileData function to "user.sub"
           //The model for getProfileData only finds by Id, it does not create a profile, (which is want we want here)
           //We may need another route that will create it.  Not sure what Jake/Ash wanna do.
           if (!user) {
             return;
           }
-          getProfileData(user.sub, isAuthenticated, args.token)
-            .then(res => args.setProfile(res))
-            .catch(err => console.log(err));
+
+          // setProfile(user);
+
+          setProfile({
+            email: user.email,
+            name: user.name,
+            id: user.sub,
+          });
+
+          // getProfileData(user.sub, isAuthenticated, args.token) This is Trying to gather information from our own api but now auth0 is responsible for giving us the information about the user.
+          //   .then(res => {
+          //     args.setProfile(res);
+          //     console.log(args);
+          //   })
+          //   .catch(err => console.log(err));
         })
         .catch(err => console.log(err));
     }
-  }, [getAccessTokenSilently, isAuthenticated, user, args]);
+  }, [
+    getAccessTokenSilently,
+    isAuthenticated,
+    user,
+    token,
+    setAuthToken,
+    setProfile,
+  ]);
 
   return (
     <Route
