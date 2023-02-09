e4ac9fab08f916f29343a69f9fd3290b229c3bca
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-08 19:27:09 -0800

    adds layout imports and footer to page

--- src/components/pages/Resources/RenderResources.js
+++ src/components/pages/Resources/RenderResources.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import DashHeader from '../../common/DashHeader';
+import { Layout } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 export default function RenderResources() {
-  return <DashHeader />;
+  return (
+    <div className="RenderResources">
+      <DashHeader />
+      <div>
+        <Layout className="Layout">
+          <Footer className="Footer">
+            Family Promise of Spokane ©2022 Created by BloomTech Labs
+          </Footer>
+        </Layout>
+      </div>
+    </div>
+  );
 }
