47810bac12dbea71d577c59bf480b3bfb268a7d8
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-08 20:06:33 -0800

    adds content and breadcrumbs

--- src/components/pages/Resources/RenderResources.js
+++ src/components/pages/Resources/RenderResources.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import DashHeader from '../../common/DashHeader';
+import BreadCrumbs from '../../common/BreadCrumbs';
+import { useLocation } from 'react-router-dom';
+
 import { Layout } from 'antd';
 const { Header, Content, Footer } = Layout;
 
 export default function RenderResources() {
+  const location = useLocation();
+
   return (
     <div className="RenderResources">
       <DashHeader />
+      <Header className="Header">
+        <h1>Resources</h1>
+      </Header>
       <div>
         <Layout className="Layout">
+          <Content className="Content">
+            <BreadCrumbs pathname={location.pathname} />
+          </Content>
           <Footer className="Footer">
             Family Promise of Spokane ©2022 Created by BloomTech Labs
           </Footer>
