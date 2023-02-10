b910baefbb2f98b90af98e562b4579fff5c6082a
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2023-02-09 19:47:58 -0800

    added antd components

--- src/components/common/FinancesIntake.jsx
+++ src/components/common/FinancesIntake.jsx
@@ -1,19 +1,97 @@
 import React from 'react';
+import { Form, Input, Checkbox } from 'antd';
 
 export const Finances = () => {
   return (
     <>
       <div className="Container">
-        <form>
-          <div className="financeHeader"></div>
-          <div className="familyHistory">
+        <Form>
+          <header className="financeHeader">
+            <h2>FINANCES</h2>
+          </header>
+
+          <section className="finance-group1">
+            <label>Family Have History of:</label>
             <label>
-              <input type="checkbox" />
+              <Checkbox />
               Eviction
             </label>
-          </div>
-          <div className="typesOfDebt"></div>
-        </form>
+            <label>
+              <Checkbox />
+              Landlord Debt
+            </label>
+            <label>
+              <Checkbox />
+              Criminal History
+            </label>
+            <label>
+              <Checkbox />
+              Poor Credit
+            </label>
+            <label>
+              <Checkbox />
+              No Rental History
+            </label>
+
+            <label>Source of income:</label>
+            <label>
+              <Checkbox />
+              TANF
+            </label>
+            <label>
+              <Checkbox />
+              SSI
+            </label>
+            <label>
+              <Checkbox />
+              SSDI
+            </label>
+            <label>
+              <Checkbox />
+              Child Support
+            </label>
+            <label>
+              Amount:
+              <Input />
+            </label>
+          </section>
+
+          <section className="finance-group2">
+            <label>Types of Debt:</label>
+            <label>
+              <Checkbox />
+              Student Loan
+            </label>
+            <label>
+              Amount:
+              <Input />
+            </label>
+            <label>
+              <Checkbox />
+              Credit Card
+            </label>
+            <label>
+              Amount:
+              <Input />
+            </label>
+            <label>
+              <Checkbox />
+              Medical Bills
+            </label>
+            <label>
+              Amount:
+              <Input />
+            </label>
+            <label>
+              <Checkbox />
+              Auto Loan
+            </label>
+            <label>
+              Amount:
+              <Input />
+            </label>
+          </section>
+        </Form>
       </div>
     </>
   );
