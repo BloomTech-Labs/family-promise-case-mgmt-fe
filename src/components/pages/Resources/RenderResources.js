1cd2944ec8e24495d6b9d68486ec78bcacc8bd72
kim5981 <kimberlyruthrodriguez@gmail.com>
2023-02-10 19:31:18 -0800

    adds resource summary and details + styling

--- src/components/pages/Resources/RenderResources.js
+++ src/components/pages/Resources/RenderResources.js
@@ -73,11 +73,24 @@ export default function RenderResources() {
             </section>
             <section className="CurrentResource">
               <h3> Current Resource Sample </h3>
+
+              <h4> Summary </h4>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip
+                nisi ut aliquip tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip
+              </p>
+              <h4> Details </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip
               </p>
 
               <a href="#" className="resource-link">
