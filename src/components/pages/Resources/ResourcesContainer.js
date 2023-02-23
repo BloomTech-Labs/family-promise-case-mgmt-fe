3986167c04c52187cd6ae9011f649a76ba0c8ea6
Esmodea Burk <esmodearburk@gmail.com>
2023-02-23 16:25:40 -0600

    FP-97 Added Header and Footer to ResourcesContainer component

--- src/components/pages/Resources/ResourcesContainer.js
+++ src/components/pages/Resources/ResourcesContainer.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Layout, Card } from 'antd';
+import DashHeader from '../../common/DashHeader';
+const { Content, Footer, Header } = Layout;
 
 const Resources = () => {
-  return <></>;
+  return (
+    <div className="RenderLayoutTemplate">
+      <Layout className="Layout">
+        <Header className="Header">
+          <DashHeader />
+        </Header>
+        <Content className="Content">
+          <div className="Content__Container">
+            <Card className="Content__Container-Card">
+              {/* <div style={{height: '100%'}}></div> */}
+            </Card>
+          </div>
+        </Content>
+        <Footer className="Footer">
+          Family Promise of Spokane ©2022 Created by BloomTech Labs
+        </Footer>
+      </Layout>
+    </div>
+  );
 };
 
 export default Resources;
