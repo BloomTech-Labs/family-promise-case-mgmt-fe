913dc0782bfbf10d7264052ff90c39c756bff328
Esmodea Burk <esmodearburk@gmail.com>
2023-02-24 17:42:54 -0600

    FP-97 Began refactoring of table using antd

--- src/components/common/Resources/ResourcesTable.js
+++ src/components/common/Resources/ResourcesTable.js
@@ -1,40 +1,46 @@
 import React, { useState } from 'react';
-// import data from './resourcesDataTemp';
+import { Table } from 'antd';
+import { dataSource, columns } from './resourcesDataTemp';
 
 const ResourcesTable = ({ setDetails }) => {
-  const [resources, setResources] = useState([]);
-  const handleDetails = details => {
-    setDetails(details);
-  };
+  // const [resources, setResources] = useState(dataSource);
+  // const handleDetails = details => {
+  //   setDetails(details);
+  // };
   return (
-    <table>
-      <tr>
-        <th>ORGANIZATION</th>
-        <th>PHONE NUMBER</th>
-        <th>RESOURCE TYPE</th>
-        <th>QUALIFICATIONS</th>
-        <th></th>
-      </tr>
-      {resources[0] ? (
-        resources.map(resource => {
-          return (
-            <tr>
-              <td>{resource.organization}</td>
-              <td>{resource.phoneNumber}</td>
-              <td>{resource.resourceType}</td>
-              <td>{resource.qualifications ? 'yes' : 'no'}</td>
-              <td>
-                <button onClick={() => handleDetails(resource.details)}>
-                  Details
-                </button>
-              </td>
-            </tr>
-          );
-        })
-      ) : (
-        <p>Oops there are no Resources.</p>
-      )}
-    </table>
+    // <table className='Resources__Table'>
+    //   <tr>
+    //     <th>ORGANIZATION</th>
+    //     <th>PHONE NUMBER</th>
+    //     <th>RESOURCE TYPE</th>
+    //     <th>QUALIFICATIONS</th>
+    //     <th></th>
+    //   </tr>
+    //   {resources[0] ? (
+    //     resources.map(resource => {
+    //       return (
+    //         <tr>
+    //           <td>{resource.organization}</td>
+    //           <td>{resource.phoneNumber}</td>
+    //           <td>{resource.resourceType}</td>
+    //           <td>{resource.qualifications ? 'yes' : 'no'}</td>
+    //           <td>
+    //             <button onClick={() => handleDetails(resource.details)}>
+    //               Details
+    //             </button>
+    //           </td>
+    //         </tr>
+    //       );
+    //     })
+    //   ) : (
+    //     <p>Oops there are no Resources.</p>
+    //   )}
+    // </table>
+    <Table
+      className="Resources__Table"
+      dataSource={dataSource}
+      columns={columns}
+    />
   );
 };
 
