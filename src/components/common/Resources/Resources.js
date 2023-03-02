e9a9175da1bb09d0ad2f31b89fb2b77346296123
Esmodea Burk <esmodearburk@gmail.com>
2023-03-01 18:22:09 -0600

    FP-97 Changed to list and card view with filter.

--- src/components/common/Resources/Resources.js
+++ src/components/common/Resources/Resources.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react';
-import ResourcesTable from './ResourcesTable';
+import ResourcesDisplay from './ResourcesDisplay';
 import DetailsView from './DetailsView';
 
 const Resources = () => {
-  const [details, setDetails] = useState('');
-  console.log(details);
   return (
     <div className="Resources">
-      <div className="Resources__LeftSide">
-        <h2 className="Resources__Title">Resources</h2>
-        <ResourcesTable setDetails={setDetails} />
-        <button className="Resources__Button">ADD RESOURCE</button>
-      </div>
-      <div className="Resources__RightSide">
-        <h2 className="Resources__Title">Details View</h2>
-        <DetailsView details={details} />
-      </div>
+      <ResourcesDisplay />
     </div>
   );
 };
