d2ddba704b22687d9a02d833ccde6bb6ffb3455c
derekjpeters <55850377+derekjpeters@users.noreply.github.com>
2023-03-02 21:56:22 -0500

    FP-104 Added ResourcesDataTemp.js for datasource (#82)
    Co-authored-by: Derek Peters <derekpeters@MacBook-Pro-57.local>

---
+++ src/components/common/Resources/resourcesDataTemp.js
@@ -0,0 +1,21 @@
+export const dataSource = [
+  {
+    title: 'Resource 1',
+    content: {
+      phoneNumber: '555-555-1234',
+      resourceType: 'Type A',
+      qualifications: 'Yes',
+      details: 'Lorem ipsum dolor sit amet',
+    },
+  },
+  {
+    title: 'Resource 2',
+    content: {
+      phoneNumber: '555-555-5678',
+      resourceType: 'Type B',
+      qualifications: 'No',
+      details: 'Consectetur adipiscing elit',
+    },
+  },
+  // Add more data here as needed
+];
