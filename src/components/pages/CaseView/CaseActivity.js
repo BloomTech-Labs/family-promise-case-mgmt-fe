2e3eb8d8e7f3c0d9f69495bc62c220f991f9c5da
Caleb Campbell <calebcampbellcrm@gmail.com>
2023-03-09 10:41:44 -0700

    changed from inline styles to less styling

--- src/components/pages/CaseView/CaseActivity.js
+++ src/components/pages/CaseView/CaseActivity.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../../styles/less/styles.less';
 
 export default function CaseActivity() {
   // When using real data, please comment out the dummyData, and initialize the caseHistoryData to the history props passed in.
@@ -20,8 +21,8 @@ export default function CaseActivity() {
 
   return (
     <>
-      <div>
-        <button style={{ marginLeft: '1em' }} onClick={toggleExpandedView}>
+      <div className="CaseActivity">
+        <button className="CaseActivity_button" onClick={toggleExpandedView}>
           View Case Activity
         </button>
       </div>
@@ -35,89 +36,31 @@ export default function CaseActivity() {
   );
 }
 
-// A custom styles object is used to not clog or interfere with the css files used site-wide, and allowing localized styling changes.
-const expandedStyles = {
-  div: {
-    marginTop: '2em',
-    display: 'absolute',
-    width: '100%',
-    top: '0',
-    left: '0',
-    backgroundColor: 'black',
-  },
-  titleDiv: {
-    backgroundColor: 'rgb(51,51,51, .90)',
-    margin: 0,
-    width: '100%',
-    padding: 0,
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-  h3: {
-    fontSize: '24px',
-    textAlign: 'center',
-    color: '#006FBA',
-    marginLeft: '2em',
-  },
-  closeButton: {},
-  legendDiv: {
-    width: '100%',
-    height: '4em',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: 'rgb(51,51,51, .99)',
-    marginTop: '.5em',
-    marginBottom: '.5em',
-  },
-  legendP: {
-    fontSize: '1.2em',
-    color: '#006FBA',
-    fontWeight: '500',
-    textAlign: 'center',
-  },
-  itemDiv: {
-    width: '100%',
-    height: '4em',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#6E6E6E',
-    marginTop: '.5em',
-    marginBottom: '.5em',
-  },
-  itemP: {
-    color: 'white',
-    fontWeight: '400',
-    textAlign: 'center',
-  },
-};
-
 const ExpandedActivity = ({ toggleExpandedView, caseHistoryData }) => {
   return (
-    <div style={expandedStyles.div}>
-      <div style={expandedStyles.titleDiv}>
-        <h3 style={expandedStyles.h3}>Case Activity</h3>
-        <button onClick={toggleExpandedView} style={expandedStyles.closeButton}>
-          close
-        </button>
-      </div>
-      <div>
-        <div style={expandedStyles.legendDiv}>
-          <p style={expandedStyles.legendP}>Modification</p>
-          <p style={expandedStyles.legendP}>Date</p>
+    <div className="ExpandedCaseActivity">
+      <div className="ExpandedCaseActivity_container">
+        <div className="ExpandedCaseActivity_title-wrap">
+          <h3 className="ExpandedCaseActivity_title">Case Activity</h3>
+          <button onClick={toggleExpandedView}>close</button>
         </div>
-        {caseHistoryData.map(item => {
-          return (
-            <>
-              <div style={expandedStyles.itemDiv}>
-                <p style={expandedStyles.itemP}>{item.action}</p>
-                <p style={expandedStyles.itemP}>{item.date}</p>
-              </div>
-            </>
-          );
-        })}
-        ;
+        <div>
+          <div className="ExpandedCaseActivity_legend-wrap">
+            <p className="ExpandedCaseActivity_legend">Modification</p>
+            <p className="ExpandedCaseActivity_legend">Date</p>
+          </div>
+          {caseHistoryData.map(item => {
+            return (
+              <>
+                <div className="ExpandedCaseActivity_item-wrap">
+                  <p className="ExpandedCaseActivity_item">{item.action}</p>
+                  <p className="ExpandedCaseActivity_item">{item.date}</p>
+                </div>
+              </>
+            );
+          })}
+          ;
+        </div>
       </div>
     </div>
   );
