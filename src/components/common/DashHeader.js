3e553a77b87c75bc7939362d263af0f31e4bca8a
derekjpeters <55850377+derekjpeters@users.noreply.github.com>
2023-03-22 21:50:43 -0400

    Updated Dashheader and fixed spacing issues (#91)
    Co-authored-by: derekjpeters <derek.peters@bloomtech.com>

--- src/components/common/DashHeader.js
+++ src/components/common/DashHeader.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import styled from 'styled-components';
 import fpLogo from '../../assets/fplogo.png';
 import PlaceholderProfileImage from '../../assets/PlaceholderProfileImage.png';
 // import FormInput from './FormInput';
 import SearchBar from './SearchBar/SearchBar';
 
+const DashHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+`;
+
+const Logo = styled.img`
+  height: 40px;
+`;
+
+const ProfileImage = styled.img`
+  height: 40px;
+  border-radius: 50%;
+`;
+
 const DashHeader = () => {
   return (
+<<<<<<< HEAD
     <div>
       <div className="DashHeader">
         <img
@@ -34,32 +52,34 @@ const DashHeader = () => {
         mode="horizontal"
         defaultSelectedKeys={['2']}
       >
+=======
+    <>
+      <DashHeaderContainer>
+        <Logo src={fpLogo} alt="Family Promise of Spokane" />
+        <ProfileImage src={PlaceholderProfileImage} alt="Profile" />
+      </DashHeaderContainer>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['dashboard']}>
+>>>>>>> 0473dc2 (Updated Dashheader and fixed spacing issues (#91))
         <Menu.Item key="dashboard">
-          <Link to="dashboard" />
-          Dashboard
+          <Link to="dashboard">Dashboard</Link>
         </Menu.Item>
-        <Menu.Item key="calandar">
-          <Link to="/calendar" />
-          Calendar
+        <Menu.Item key="calendar">
+          <Link to="/calendar">Calendar</Link>
         </Menu.Item>
         <Menu.Item key="cases">
-          <Link to="/cases" />
-          Cases
+          <Link to="/cases">Cases</Link>
         </Menu.Item>
         <Menu.Item key="resources">
-          <Link to="resources" />
-          Resources
+          <Link to="resources">Resources</Link>
         </Menu.Item>
         <Menu.Item key="services">
-          <Link to="/services" />
-          Services
+          <Link to="/services">Services</Link>
         </Menu.Item>
         <Menu.Item key="client-intake-form">
-          <Link to="client-intake-form" />
-          Intake
+          <Link to="client-intake-form">Intake</Link>
         </Menu.Item>
       </Menu>
-    </div>
+    </>
   );
 };
 
