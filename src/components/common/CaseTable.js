8daa2638667aba261f2b0ad72c10ac87bdd3196a
mgrobstein <mgrobst1@binghamton.edu>
2023-04-03 10:09:59 -0400

    updated caseTable

--- src/components/common/CaseTable.js
+++ src/components/common/CaseTable.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import { Table, Tag } from 'antd';
 
-const data = [
-  {
-    key: '1',
-    clientName: 'John Doe',
-    caseType: 'Housing',
-    status: 'Active',
-    dateOpened: '2022-01-01',
-    assignedTo: 'Jane Smith',
-  },
-  {
-    key: '2',
-    clientName: 'Jane Smith',
-    caseType: 'Employment',
-    status: 'Inactive',
-    dateOpened: '2022-02-01',
-    assignedTo: 'John Doe',
-  },
-  {
-    key: '3',
-    clientName: 'Bob Johnson',
-    caseType: 'Housing',
-    status: 'Active',
-    dateOpened: '2022-03-01',
-    assignedTo: 'Jane Smith',
-  },
-];
-
 const CaseTable = () => {
+  const dataSource = [
+    {
+      key: '1',
+      clientName: 'John Doe',
+      caseType: 'Housing',
+      status: 'Active',
+      dateOpened: '2022-01-01',
+      assignedTo: 'Jane Smith',
+    },
+    {
+      key: '2',
+      clientName: 'Jane Smith',
+      caseType: 'Employment',
+      status: 'Inactive',
+      dateOpened: '2022-02-01',
+      assignedTo: 'John Doe',
+    },
+    {
+      key: '3',
+      clientName: 'Bob Johnson',
+      caseType: 'Housing',
+      status: 'Active',
+      dateOpened: '2022-03-01',
+      assignedTo: 'Jane Smith',
+    },
+  ];
+
   const columns = [
     {
       title: 'Client Name',
@@ -44,9 +44,6 @@ const CaseTable = () => {
       title: 'Status',
       dataIndex: 'status',
       key: 'status',
-      render: text => (
-        <Tag color={text === 'Active' ? 'green' : 'red'}>{text}</Tag>
-      ),
     },
     {
       title: 'Date Opened',
@@ -58,9 +55,24 @@ const CaseTable = () => {
       dataIndex: 'assignedTo',
       key: 'assignedTo',
     },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phoneNumber',
+      key: 'phoneNumber',
+    },
+    {
+      title: "Client's Email Address",
+      dataIndex: 'clientEmail',
+      key: 'clientEmail',
+    },
+    {
+      title: 'Additional Notes',
+      dataIndex: 'notes',
+      key: 'notes',
+    },
   ];
 
-  return <Table columns={columns} dataSource={data} />;
+  return <Table dataSource={dataSource} columns={columns} />;
 };
 
 export default CaseTable;
