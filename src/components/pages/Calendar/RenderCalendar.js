00e5dcc47df54a5adc911de0551f18f15c9e61ef
Matt Grobstein <mgrobst1@binghamton.edu>
2023-04-06 14:04:12 -0400

    FP-101 Completed (#98)

--- src/components/pages/Calendar/RenderCalendar.js
+++ src/components/pages/Calendar/RenderCalendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DashHeader from '../../common/DashHeader';
 import SearchIcon from './SearchIcon';
 import {
@@ -12,9 +12,12 @@ import {
   ConfigProvider,
   List,
   Modal,
+  TimePicker,
 } from 'antd';
 import moment from 'moment';
 import 'antd/dist/antd.dark.css';
+import axios from 'axios';
+import dayjs from 'dayjs';
 
 const { Sider, Content, Footer } = Layout;
 const weekFormat = 'MM/DD';
@@ -24,36 +27,99 @@ const customWeekStartEndFormat = value =>
     .format(weekFormat)}`;
 
 function RenderCalendar() {
+  const dateCellRender = currentDate => {
+    const date = currentDate.format('YYYY-MM-DD');
+    const events = getEventsByDate(date);
+    return (
+      <ul className="events">
+        {events.map(event => (
+          <li key={event.title}>
+            <span className="event-title">{event.title}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const getEventsByDate = date => {
+    return events.filter(event => {
+      return dayjs(event.start).format('YYYY-MM-DD') === date;
+    });
+  };
+
+  const getURL = 'https://fp-case-mgmt-api.herokuapp.com/api/google_events';
+  const postURL = 'https://fp-case-mgmt-api.herokuapp.com/api/google_events/1';
+
+  useEffect(() => {
+    const ar = [];
+    axios
+      .get(getURL)
+      .then(res => {
+        console.log(res);
+        setEvents(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   const [selectedDate, setSelectedDate] = useState(moment());
-  const [events, setEvents] = useState([]); // Initialize an empty array to store events
-  const [isModalVisible, setIsModalVisible] = useState(false); // Initialize the modal visibility state
+  const [events, setEvents] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [newEvent, setNewEvent] = useState({
     title: '',
     date: moment(),
-  }); // Initialize the new event data state
+  });
 
   const handleDateChange = value => {
     setSelectedDate(value);
   };
 
   const handleCreateEventClick = () => {
-    setIsModalVisible(true); // Show the modal when the "CREATE EVENT" button is clicked
+    setIsModalVisible(true);
   };
 
   const handleModalOk = () => {
+    axios
+      .post(postURL, { eventData })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    const start =
+      newEvent.startDate.format('YYYY-MM-DD') +
+      'T' +
+      newEvent.startTime.format('HH:mm:ss') +
+      'Z';
+    const end =
+      newEvent.endDate.format('YYYY-MM-DD') +
+      'T' +
+      newEvent.endTime.format('HH:mm:ss') +
+      'Z';
+
     const eventData = {
       title: newEvent.title,
-      date: newEvent.date,
-      // TODO: Add other event data fields as needed
+      description: newEvent.description,
+      location: newEvent.location,
+      start: start,
+      end: end,
     };
-    setEvents([...events, eventData]); // Add the new event data to the events array
-    setIsModalVisible(false); // Hide the modal when the user submits the event details
+    console.log(eventData);
+    setEvents([...events, eventData]);
+    setIsModalVisible(false);
     setNewEvent({
-      // Reset the new event data state
       title: '',
       date: moment(),
+      location: '',
+      description: '',
+      startDate: '',
+      startTime: '',
+      endDate: '',
+      endTime: '',
     });
-    console.log('New event created:', eventData); // Log the new event data to the console
+    console.log('New event created:', eventData);
   };
 
   const handleModalCancel = () => {
@@ -61,6 +127,12 @@ function RenderCalendar() {
     setNewEvent({
       title: '',
       date: moment(),
+      location: '',
+      description: '',
+      startDate: '',
+      starTime: '',
+      endDate: '',
+      endTime: '',
     });
   };
 
@@ -111,18 +183,21 @@ function RenderCalendar() {
               </div>
             </Sider>
             <Content className="WeeklyCalendar">
-              <Calendar value={selectedDate} onChange={handleDateChange} />
+              <Calendar
+                dateCellRender={dateCellRender}
+                value={selectedDate}
+                onChange={handleDateChange}
+              />
               <List
                 header={<div>Events</div>}
                 dataSource={events.filter(event =>
-                  event.date.isSame(selectedDate, 'day')
+                  moment(event.start).isSame(selectedDate, 'day')
                 )}
                 renderItem={item => (
                   <List.Item>
                     <div>
                       <span>{item.title}</span>
                       <span>{item.date.format('dddd, MMMM Do YYYY')}</span>
-                      {/* TODO: Display other event data fields as needed */}
                     </div>
                   </List.Item>
                 )}
@@ -134,7 +209,6 @@ function RenderCalendar() {
           </Footer>
         </Layout>
 
-        {/* Modal for creating new events */}
         <Modal
           title="Create Event"
           visible={isModalVisible}
@@ -142,20 +216,69 @@ function RenderCalendar() {
           onCancel={handleModalCancel}
         >
           <Form>
-            <Form.Item label="Title">
+            <Form.Item label={<label style={{ color: 'white' }}>Title</label>}>
               <Input
                 placeholder="Event title"
                 value={newEvent.title}
                 onChange={e => handleNewEventChange('title', e.target.value)}
               />
             </Form.Item>
-            <Form.Item label="Date">
-              <DatePicker
-                value={newEvent.date}
-                onChange={value => handleNewEventChange('date', value)}
+            <Form.Item
+              label={<label style={{ color: 'white' }}>Location</label>}
+            >
+              <Input
+                placeholder="Event Location"
+                value={newEvent.location}
+                onChange={e => handleNewEventChange('location', e.target.value)}
               />
             </Form.Item>
-            {/* TODO: Add other form fields as needed */}
+            <Form.Item
+              label={<label style={{ color: 'white' }}>Description</label>}
+            >
+              <Input
+                placeholder="Event description"
+                value={newEvent.description}
+                onChange={e =>
+                  handleNewEventChange('description', e.target.value)
+                }
+              />
+            </Form.Item>
+            <Form.Item
+              label={<label style={{ color: 'white' }}>Start Date</label>}
+            >
+              <DatePicker
+                format="YYYY-MM-DD"
+                value={newEvent.startDate}
+                onChange={value => handleNewEventChange('startDate', value)}
+              />
+            </Form.Item>
+            <Form.Item
+              label={<label style={{ color: 'white' }}>Start Time</label>}
+            >
+              <TimePicker
+                format="HH:mm:ss"
+                value={newEvent.startTime}
+                onChange={value => handleNewEventChange('startTime', value)}
+              />
+            </Form.Item>
+            <Form.Item
+              label={<label style={{ color: 'white' }}>End Date</label>}
+            >
+              <DatePicker
+                format="YYYY-MM-DD"
+                value={newEvent.endDate}
+                onChange={value => handleNewEventChange('endDate', value)}
+              />
+            </Form.Item>
+            <Form.Item
+              label={<label style={{ color: 'white' }}>End Time</label>}
+            >
+              <TimePicker
+                format="HH:mm:ss"
+                value={newEvent.endTime}
+                onChange={value => handleNewEventChange('endTime', value)}
+              ></TimePicker>
+            </Form.Item>
           </Form>
         </Modal>
       </ConfigProvider>
